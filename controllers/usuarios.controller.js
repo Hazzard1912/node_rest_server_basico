b4aa213ea12a7876a5f18ee75ada07cef1df6898
Jhonnier <jhonnier.hernandez@correounivalle.edu.co>
2023-04-23 13:12:39 -0500

    .env borrado

--- controllers/usuarios.controller.js
+++ controllers/usuarios.controller.js
@@ -1,27 +1,73 @@
 const { response } = require("express");
+const bcryptjs = require("bcryptjs");
 
-const usuariosGet = (req, res = response) => {
+const Usuario = require("../models/usuario");
 
-  const { q, nombre = 'No name', apikey } = req.query;
+const usuariosGet = async (req, res = response) => {
+  //const { q, nombre = "No name", apikey } = req.query;
+  const { limit = 5, from = 0 } = req.query;
+  const query = { estado: true };
 
-  res.json({ message: "get api - controlador", q, nombre, apikey });
+  // De esta manera lanzamos de manera simultanea estas consultas que
+  // a su vez son promesas. Es eficiente en este ejemplo ya que las dos
+  // peticiones no dependen la una de la otra, luego no tiene sentido que
+  // esperemos a que finalice una para comenzar la otra.
+
+  // Desestructuramos los arreglos en el orden que los pusimos
+  const [total, usuarios] = await Promise.all([
+    Usuario.countDocuments(query),
+    Usuario.find(query).skip(Number(from)).limit(Number(limit)),
+  ]);
+
+  // Probablemente, esa condicion que enviamos en estos metodos que
+  // abstraen consultas sql hacen las veces del where donde filtramos
+  // la consulta respecto a una condicion.
+
+  res.json({ total, usuarios });
 };
 
-const usuariosPut = (req, res = response) => {
-
+const usuariosPut = async (req, res = response) => {
   const id = req.params.id;
+  const { _id, password, google, correo, ...info } = req.body;
 
-  res.json({ message: "put api - controlador", id });
+  // Validar contra base de datos:
+  if (password) {
+    const saltRounds = bcryptjs.genSaltSync();
+    info.password = bcryptjs.hashSync(password, saltRounds);
+  }
+
+  const usuario = await Usuario.findByIdAndUpdate(id, info);
+
+  res.json(usuario);
 };
 
-const usuariosPost = (req, res = response) => {
-  const { nombre, edad } = req.body;
-  res.json({ message: "post api - controlador", nombre, edad });
+const usuariosPost = async (req, res = response) => {
+  const { nombre, correo, password, rol } = req.body;
+  const usuario = new Usuario({ nombre, correo, password, rol });
+
+  const saltRounds = bcryptjs.genSaltSync();
+  usuario.password = bcryptjs.hashSync(password, saltRounds);
+
+  await usuario.save();
+
+  res.json(usuario);
 };
 
-const usuariosDelete = (req, res = response) => {
-  res.json({ message: "delete api - controlador" });
+const usuariosDelete = async (req, res = response) => {
+  const { id } = req.params;
+
+  // Borrando fisicamente (No es recomendado)
+  //const usuario = await Usuario.findByIdAndDelete(id);
+
+  // Cambiando estado del usuario para evitar la posible integridad 
+  // referencial, por lo tanto eliminar en realidad es un update.
+  const usuario = await Usuario.findByIdAndUpdate( id, {estado: false});
+
+  res.json(usuario);
 };
+
+// Hemos finalizado una api rest, conectada a base de datos y con todo su
+// CRUD definido.
 
 const usuariosPatch = (req, res = response) => {
   res.json({ message: "patch api - controlador" });
--- routes/usuarios.routes.js
+++ routes/usuarios.routes.js
@@ -1,15 +1,55 @@
 const { Router } = require("express");
-const { usuariosGet, usuariosPut, usuariosPost, usuariosDelete, usuariosPatch } = require("../controllers/usuarios.controller");
+const {
+  usuariosGet,
+  usuariosPut,
+  usuariosPost,
+  usuariosDelete,
+  usuariosPatch,
+} = require("../controllers/usuarios.controller");
+const { check } = require("express-validator");
+const { validarCampos } = require("../middlewares/validar-campos");
+const {
+  esRoleValido,
+  existeEmail,
+  existeUsuarioPorId,
+} = require("../helpers/db-validators");
 
 const router = Router();
 
 router.get("/", usuariosGet);
 
-router.put("/:id", usuariosPut);
+router.put(
+  "/:id",
+  [
+    // Podemos tomar el id del segmento directamente desde el check
+    
+    check("rol").custom(esRoleValido),
+    validarCampos,
+  ],
+  usuariosPut
+);
 
-router.post("/",  usuariosPost);
+router.post(
+  "/",
+  [
+    check("nombre", "El nombre es obligatorio").not().isEmpty(),
+    check("correo", "El correo no es valido").isEmail(),
+    check("correo").custom(existeEmail),
+    check("password", "El password debe de ser mas de 6 letras").isLength({
+      min: 6,
+    }),
+    check("rol").custom(esRoleValido),
+    //check('rol', 'No es un rol valido').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+    validarCampos,
+  ],
+  usuariosPost
+);
 
-router.delete("/", usuariosDelete);
+router.delete("/:id", [
+    check("id", "No es un id valido").isMongoId(),
+    check("id").custom(existeUsuarioPorId),
+    validarCampos,
+], usuariosDelete);
 
 router.patch("/", usuariosPatch);
 
